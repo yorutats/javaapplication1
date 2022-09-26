f07e77d3faf002ff6136ba543d32a9076dd8b950
yichu <82928556+yorutats@users.noreply.github.com>
2022-09-26 22:18:41 +0800

    Add files via upload

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,63 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+        System.out.println("23333");
+        
+        // *變數的生命週期-宣告 配置 初始化 回收*
+        
+        int a; //生命週期-宣告i、配置int的空間給i
+        a=5; //生命週期-初始化
+       
+        System.out.println(a);
+        
+        char ttt = 'A';
+        
+        int grade=70;
+        
+        if (grade >=60){
+            System.out.println("通過");
+        }else
+        {
+            System.out.println("不通過");
+        }
+        
+        int sum=0;
+        for(int i=1;i<=10;i=i+1)//起始條件;終止條件;變化條件 
+        {
+            if(i%4==1 || i%4==0)
+            {
+                sum=sum+1;
+            }
+            else
+            {
+                sum=sum+1;
+            }    
+        }
+        System.out.println(sum);
+        
+        int i; //變數要在區塊錢宣告
+        for(i=1;i<=10;i=i+1)//起始條件;終止條件;變化條件 
+        {
+            if(i%4==1 || i%4==0)
+            {
+                sum=sum+1;
+            }
+            else
+            {
+                sum=sum+1;
+            }    
+        }
+        System.out.println(i); 
+        
+        int b=3;
+        float j=(float)3.3; //or 3.3f，否則4bytes的float放不下3.3 
+        double k=3.3;
+        //jave左右兩邊一定是相同的資料型別
+        
+        //float j=(float)3.3;//大型別轉小型別-DownCasting
+        //小變大會自動加入-UpCasting
+        
+    } //生命週期-回收
     
 }
